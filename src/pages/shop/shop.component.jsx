a993c8982e85b02b362ee55601062f3c88394932
Rahil Khan <rahil4rahil@gmail.com>
2020-09-18 02:39:16 +0530

    Header Added

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,28 +7,28 @@ import './shop.style.scss'
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collections: SHOP_DATA
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+  render() {
+    const { collections } = this.state
+    return (
+      <div className='shop-page'>
+        {
+          collections.map(({ id, ...otherCollectionProps }) => {
+            return (
+              <CollectionPreview key={id} {...otherCollectionProps} />
+            )
+          })
         }
-    }
-
-    render() {
-        const { collections } = this.state
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => {
-                        return (
-                            <CollectionPreview key={ id } {...otherCollectionProps} />
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default ShopPage
