4f3f737e248a2e81bdb794fe66c733aa84c08028
Rahil Khan <rahil4rahil@gmail.com>
2020-09-18 02:19:50 +0530

    Collection Item, Collection Preview & Shop Page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import { Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/shop/hats' component={HatsPage}></Route>
+      <Route exact path='/shop' component={ShopPage}></Route>
       {/* <HomePage /> */}
     </div>
   ); 
